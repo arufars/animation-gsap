4768d381cc59bd549c3cbe740b1df9c8f071f42c
Marineux <firselee@gmail.com>
2022-01-17 22:42:48 +0700

    unlock gsap animation

---
+++ script/app.js
@@ -0,0 +1,49 @@
+
+
+gsap.from(".logo-box", {
+    opacity: 0,
+    duration: .5,
+    delay: 2,
+    y:10
+});
+gsap.from(".btn-action", {
+    opacity: 0,
+    duration: .4,
+    delay: 3.2,
+    y:10
+});
+gsap.from(".nav-item", {
+    opacity: 0,
+    duration: .8,
+    delay: 2.2,
+    y: 20,
+    stagger: 0.2
+});
+
+gsap.from(".hero-title", {
+    opacity: 0,
+    duration: 1,
+    delay: 1.9,
+    y:30
+});
+gsap.from(".hero-descp", {
+    opacity: 0,
+    duration: 1,
+    delay: 2.2,
+    y: 30
+});
+gsap.from(".hero-button", {
+    opacity: 0,
+    duration: 1,
+    delay: 3.2,
+    x: -30
+});
+
+
+gsap.from(".hero-img", {
+    opacity: 0,
+    duration: 1,
+    delay: 3.2,
+    // scaleX: 1,
+    x: 30
+});
