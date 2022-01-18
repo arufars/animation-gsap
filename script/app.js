871919888fe60ddf1cecce0d9dd0cce1a86906be
Marineux <firselee@gmail.com>
2022-01-18 13:39:48 +0700

    add transition powerful

--- script/app.js
+++ script/app.js
@@ -1,14 +1,13 @@
 
-
 gsap.from(".logo-box", {
     opacity: 0,
-    duration: .5,
+    duration: .8,
     delay: 2,
     y:10
 });
 gsap.from(".btn-action", {
     opacity: 0,
-    duration: .4,
+    duration: .9,
     delay: 3.2,
     y:10
 });
@@ -39,10 +38,46 @@ gsap.from(".hero-button", {
     y: 30
 });
 
-gsap.from(".hero-img", {
+//  IMG
+gsap.to(".hide", {
+    x: "100%",
+    opacity: 1,
+    duration: 3.5,
+    delay: 2,
+    ease: "power4.inOut"
+});
+
+
+gsap.from(".img", {
     opacity: 0,
-    duration: 1,
-    delay: 3.2,
-    // scaleX: 1,
+    duration: 1.9,
+    scale: 2,
+    delay: 2.8,
+    ease: "power2.inOut",
     x: 30
+});
+// Akhir IMG
+
+
+// Intro
+gsap.to("span", {
+    translateY: 0,
+    stagger: 0.2,
+    ease: "power4.out"
+});
+gsap.to(".intro", {
+    // opacity: 0,
+    duration: 1,
+    delay: 1,
+    y: "-100%",
+    ease: "power4.in"
+});
+
+
+gsap.to(".wallpaper", {
+    x: "100%",
+    // opacity: 0,
+    duration: 2.5,
+    delay: .8,
+    ease: "power4"
 });
